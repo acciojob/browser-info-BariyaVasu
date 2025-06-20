82205f3fdeb09090f8b328c2a35bfd710983e81e
Bariya Vishwas <bariyavasu46@gmail.com>
2025-06-20 13:54:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-//your JS code here. If required.
-const userAgent = navigator.userAgent;
+
+    function getBrowserInfo() {
+      const userAgent = navigator.userAgent;
       let browserName = "Unknown";
       let browserVersion = "";
 
       if (/chrome|crios|crmo/i.test(userAgent) && !/edg/i.test(userAgent) && !/opr|opera/i.test(userAgent)) {
         browserName = "Google Chrome";
-        browserVersion = userAgent.match(/Chrome\/([\d.]+)/)[1];
+        browserVersion = userAgent.match(/Chrome\/([\d.]+)/)?.[1] || "";
       } else if (/firefox|fxios/i.test(userAgent)) {
         browserName = "Mozilla Firefox";
-        browserVersion = userAgent.match(/Firefox\/([\d.]+)/)[1];
+        browserVersion = userAgent.match(/Firefox\/([\d.]+)/)?.[1] || "";
       } else if (/safari/i.test(userAgent) && !/chrome|crios|crmo/i.test(userAgent)) {
         browserName = "Safari";
-        browserVersion = userAgent.match(/Version\/([\d.]+)/)[1];
+        browserVersion = userAgent.match(/Version\/([\d.]+)/)?.[1] || "";
       } else if (/edg/i.test(userAgent)) {
         browserName = "Microsoft Edge";
-        browserVersion = userAgent.match(/Edg\/([\d.]+)/)[1];
+        browserVersion = userAgent.match(/Edg\/([\d.]+)/)?.[1] || "";
       } else if (/opr|opera/i.test(userAgent)) {
         browserName = "Opera";
-        browserVersion = userAgent.match(/(Opera|OPR)\/([\d.]+)/)[2];
+        browserVersion = userAgent.match(/(Opera|OPR)\/([\d.]+)/)?.[2] || "";
       }
 
       return `${browserName} (Version: ${browserVersion})`;
